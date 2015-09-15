565924150adc9ddf71c0feee67a32b23ee9bc2e2
kevinzhwl <kevinzhwl@gmail.com>
2015-09-15 13:57:34 +0800

    更新构建脚本

--- buildscript/deploy.bat
+++ buildscript/deploy.bat
@@ -4,5 +4,7 @@ xcopy /C/Y/E "%WORKSPACE%\DuiVision\include\*.*" "%RC_DIR%\include\include\"
 xcopy /C/Y/E "%WORKSPACE%\DuiVision\common\*.*" "%RC_DIR%\include\common\"
 xcopy /C/Y/E "%WORKSPACE%\DuiVision\activex\*.*" "%RC_DIR%\include\activex\"
 xcopy /C/Y/E "%WORKSPACE%\DuiVision\pugixml\*.*" "%RC_DIR%\include\pugixml\"
+xcopy /C/Y/E "%WORKSPACE%\DuiVision\third-part\*.*" "%RC_DIR%\include\third-part\"
 
 xcopy /C/Y/E "%WORKSPACE%\lib\*.*" "%RC_DIR%\lib\"
+xcopy /C/Y/E "%WORKSPACE%\bin\wke.dll" "%RC_DIR%\"
